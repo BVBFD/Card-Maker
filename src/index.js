c9a33986dcde2d43bbff8d263ca291c5b8bacd5a
BVBFD <lsevina126@gmail.com>
2021-10-02 02:05:38 +0900

    add loading spinner or fileInput

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.module.css';
-import App from './app';
-import AuthService from './service/auth_service';
-import ImageUploader from './service/image_uploader';
-import ImageFileInput from './components/image_file_input/image_file_input';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.module.css";
+import App from "./app";
+import AuthService from "./service/auth_service";
+import ImageUploader from "./service/image_uploader";
+import ImageFileInput from "./components/image_file_input/image_file_input";
 
 const authService = new AuthService();
 const imageUploader = new ImageUploader();
-const FileInput = props => (
-  <ImageFileInput {...props} imageUploader={imageUploader}/>
+const FileInput = (props) => (
+  <ImageFileInput {...props} imageUploader={imageUploader} />
 );
 // 확장성 강화를 위해서 props => (...props) 추가
 // components prop인 경우 대문자로 시작함
 
 ReactDOM.render(
   <React.StrictMode>
-    <App authService={authService} FileInput={FileInput}/>
+    <App authService={authService} FileInput={FileInput} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
